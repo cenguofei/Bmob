e8e919e655c5665d579787c5c6f4de85f09ca7f3
kim <kim@qq.com>
2022-07-03 10:01:04 +0800

    修改了注册界面

--- app/src/main/java/com/example/bmob/fragments/login/RegisterFragment.kt
+++ app/src/main/java/com/example/bmob/fragments/login/RegisterFragment.kt
@@ -1,16 +1,21 @@
 package com.example.bmob.fragments.login
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import cn.bmob.v3.BmobUser
 import com.example.bmob.R
 import com.example.bmob.databinding.FragmentRegisterBinding
+import com.example.bmob.viewmodels.BmobUserViewModel
 
 
 class RegisterFragment : Fragment() {
     private lateinit var binding:FragmentRegisterBinding
+    private val userViewModel:BmobUserViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -25,5 +30,14 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.signupBth.setOnClickListener {
+
+        }
+    }
+    private fun isAllInput():Boolean{
+        return binding.let {
+            TextUtils.isEmpty(it.nameEv.text)
+
+        }
     }
 }
--- app/src/main/java/com/example/bmob/viewmodels/BmobUserViewModel.kt
+++ app/src/main/java/com/example/bmob/viewmodels/BmobUserViewModel.kt
@@ -14,4 +14,13 @@ class BmobUserViewModel:ViewModel() {
     fun getUserInfo(callback:(Boolean, User?)->Unit){
         userRepository.getUserInfo(callback)
     }
+    //手机号码重置密码
+    //1. 请求重置密码操作的短信验证码
+    fun findPassword(phoneNumber:String,callback: (smsId:Int,error:String?) -> Unit){
+        userRepository.findPassword(phoneNumber,callback)
+    }
+    //2. 然后执行验证码的密码重置操作
+    fun verifyCode(smsId:String,newPassword:String,callback: (isResetSuccess:Boolean,msg:String) -> Unit){
+        userRepository.verifyCode(smsId,newPassword,callback)
+    }
 }
