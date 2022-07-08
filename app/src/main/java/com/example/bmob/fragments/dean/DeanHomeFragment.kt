21bb76651e46bf8a2d12ea33a86c9dc88f3763dd
kim <kim@qq.com>
2022-07-08 09:23:47 +0800

    搭建了系主任和教务长的框架1.2

--- app/src/main/java/com/example/bmob/fragments/dean/DeanHomeFragment.kt
+++ app/src/main/java/com/example/bmob/fragments/dean/DeanHomeFragment.kt
@@ -70,11 +70,11 @@ class DeanHomeFragment : Fragment(),FragmentEventListener {
                 if (adapter == null && it.second.isNotEmpty()) {
                     adapter = SearchRecyclerViewAdapter { thesis ->
                         Log.v(LOG_TAG, "回调：$thesis")
-                        val actionTeacherHomeFragmentToShowThesisFragment =
-                            TeacherHomeFragmentDirections.actionTeacherHomeFragmentToShowThesisFragment(
+                        val actionDeanHomeFragmentToShowThesisFragment =
+                            DeanHomeFragmentDirections.actionDeanHomeFragmentToShowThesisFragment(
                                 thesis,false
                             )
-                        findNavController().navigate(actionTeacherHomeFragmentToShowThesisFragment)
+                        findNavController().navigate(actionDeanHomeFragmentToShowThesisFragment)
                     }
                     adapter!!.setThesisListForFirst(it.second)
                     binding.recyclerView1.adapter = adapter
@@ -113,8 +113,6 @@ class DeanHomeFragment : Fragment(),FragmentEventListener {
     }
 
     override fun setEventListener() {
-
-
         viewModel.setSearchViewListener(binding.searchView2,binding.recyclerView1,binding.contentLinearLayout)
     }
 }
